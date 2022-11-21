6bf5a91c7cb9c8b61450cfb3f853d1efcca7f12c
Jamie Vos <Jamievos100@gmail.com>
2022-11-21 19:43:18 +0100

    JS + Scss sticky navbar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,15 +1,26 @@
+import React, { useState, useEffect, useRef } from "react";
 import "./Navigation.scss";
-import { useRef } from "react";
-import React from "react";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";   
-import useSticky from "./useSticky";
 
     
-const Navigation = (props) => {
+function Navigation (props) {
 
-    const { sticky, stickyRef } = useSticky();
+    const [stickyClass, setStickyClass] = useState('');
     const navRef = useRef();
+
+    useEffect(() => {
+        window.addEventListener('scroll', stickNavbar);
+        return () => window.removeEventListener('scroll', stickNavbar);
+      }, []);
+
+      const stickNavbar = () => {
+        if (window !== undefined) {
+          let windowHeight = window.scrollY;
+          // window height changed for the demo
+          windowHeight > 150 ? setStickyClass('sticky-nav') : setStickyClass('');
+        }
+      };
 
     const showNavbar = () => {
         navRef.current.classList.toggle("responsive__nav");
@@ -17,7 +28,7 @@ const Navigation = (props) => {
 
     return (
 
-        <header className="background header__nav">
+        <header className={`header__nav ${stickyClass}`} >
             {/* <img className="nav__logo" src={props.navigation__logo} />  */}
             <nav className="text" ref={navRef}>
                 <a href='#1'>Home</a>
