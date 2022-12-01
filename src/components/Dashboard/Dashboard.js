6c268f5f4b03be1b1723109e85e9d14071b61706
Jamie <31694@ma-web.nl>
2022-12-01 14:32:05 +0100

    Data toegevoegd

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import "./Dashboard.scss";
-import JSONDATA from '../../Data/DataShoes.json';
 import { useState } from 'react';
 
 function Dashboard() {
@@ -17,16 +16,6 @@ function Dashboard() {
             <div className="container__upper">
             <Link to="/"><i class="icons__db fa-solid fa-right-left"></i></Link>
             </div>
-
-            {JSONDATA.filter((val) => {
-                    if(searchTerm == ""){
-                        return val 
-                    } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())){
-                        return val
-                    }
-                }).map((val, key) => {
-                    return <div key={key} className="product__info" style={{backgroundImage: `url(${val.picture})`}} />
-                })}
         </div>
     </div>
     </section>
