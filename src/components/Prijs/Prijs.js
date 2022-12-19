6896a32cd24b149a8ebef5520c382d4feb7bab2d
Jamie <31694@ma-web.nl>
2022-12-19 20:33:36 +0100

    Timeline

--- src/components/Prijs/Prijs.js
+++ src/components/Prijs/Prijs.js
@@ -2,9 +2,61 @@ import "./Prijs.scss";
 
 const Prijs = (props) => {
 
+    
     return (
         <section className="seciton__prijs">
-            <h1>asd</h1>
+            <ul>
+                <li>
+                    <div>
+                        <time>Lorem</time> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus quibusdam officia possimus, dolore aliquam vel excepturi accusantium iusto deserunt qui magnam laborum quos iste, amet cupiditate error porro dolores! Amet?
+                    </div>
+                </li>
+                <li>
+                    <div>
+                    <time>Lorem</time> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus quibusdam officia possimus, dolore aliquam vel excepturi accusantium iusto deserunt qui magnam laborum quos iste, amet cupiditate error porro dolores! Amet?                    
+                    </div>
+                </li>
+                <li>
+                    <div>
+                    <time>Lorem</time> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus quibusdam officia possimus, dolore aliquam vel excepturi accusantium iusto deserunt qui magnam laborum quos iste, amet cupiditate error porro dolores! Amet?                    
+                    </div>
+                </li>
+                <li>
+                    <div>
+                    <time>Lorem</time> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus quibusdam officia possimus, dolore aliquam vel excepturi accusantium iusto deserunt qui magnam laborum quos iste, amet cupiditate error porro dolores! Amet?
+                    </div>
+                </li>
+                <li>
+                    <div>
+                    <time>Lorem</time> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus quibusdam officia possimus, dolore aliquam vel excepturi accusantium iusto deserunt qui magnam laborum quos iste, amet cupiditate error porro dolores! Amet?
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <time>1956</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <time>Lorem</time> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus quibusdam officia possimus, dolore aliquam vel excepturi accusantium iusto deserunt qui magnam laborum quos iste, amet cupiditate error porro dolores! Amet?
+                    </div>
+                </li>
+                <li>
+                    <div>
+                    <time>Lorem</time> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus quibusdam officia possimus, dolore aliquam vel excepturi accusantium iusto deserunt qui magnam laborum quos iste, amet cupiditate error porro dolores! Amet?
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <time>1977</time> Vestibulum porttitor lorem sed pharetra dignissim. Nulla maximus, dui a tristique iaculis, quam dolor convallis enim, non dignissim ligula ipsum a turpis.
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <time>Lorem</time> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus quibusdam officia possimus, dolore aliquam vel excepturi accusantium iusto deserunt qui magnam laborum quos iste, amet cupiditate error porro dolores! Amet?
+                    </div>
+                </li>
+            </ul>
         </section>
     );
 }
